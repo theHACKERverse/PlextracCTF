56afd417de524a0287b8b6e9bfb8fc7784f48402
Pentestr5044 <pentestr5044@gmail.com>
2024-01-11 16:25:06 -0800

    front end and backend
    front and backend have been confirmed functional now for design and implementation

---
+++ ctfBack/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const port = 3000;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+app.listen(port, function () {
+    console.log('Example app listening on port 3000!');
+});
